0ac2957df13d08c2e3570d618df691f163bd2144
WebDevelopmente <digbijaynayak522@gmail.com>
2022-06-29 08:52:03 +0530

    Worked on Login and Signup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 
-const LoginPage: React.FC = () => {
+const LoginPage: React.FC = ({ history }: any) => {
+  const goTo = (path: string) => {
+    history.push(path);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -14,6 +29,35 @@ const LoginPage: React.FC = () => {
             <IonTitle size="large">Login Page</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+        <form onSubmit={(e) => goTo("/tabs/home")}>
+          <IonList>
+            <IonItem
+              style={{
+                border: "1px solid black",
+                marginBottom: "5px",
+                margin: "auto",
+                width: "300px",
+              }}
+            >
+              <IonLabel>Email</IonLabel>
+              <IonInput type="email"></IonInput>
+            </IonItem>
+
+            <IonItem className="pass" id="pas">
+              <IonLabel>Password</IonLabel>
+              <IonInput type="password"></IonInput>
+            </IonItem>
+            <IonButton type="submit" style={{ marginLeft: "150px" }}>
+              Log in
+            </IonButton>
+          </IonList>
+        </form>
+        <div style={{ display: "flex", marginLeft: "60px" }}>
+          <a href="/signup">Create an account?</a>
+          &nbsp;&nbsp;
+          <a href="/reset-password">Forgot Password</a>
+        </div>
         {/* <ExploreContainer name="Tab 3 page" /> */}
       </IonContent>
     </IonPage>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,28 +1,59 @@
-import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Signup.css";
 
 const SignupPage: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader style={{ marginBottom: "5px" }}>
         <IonToolbar color="primary">
-        <IonButtons slot="start"></IonButtons>
+          <IonButtons slot="start"></IonButtons>
           <IonTitle>Signup Page</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        {/* <IonHeader collapse="condense">
+        <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Signup Page</IonTitle>
           </IonToolbar>
-        </IonHeader> */}
-        <form>
-            <IonList>
-                <IonItem>
-                    <IonLabel>Email</IonLabel>
-                </IonItem>
-            </IonList>
-        </form>
+        </IonHeader>
+        <form id="view">
+          <IonList>
+            <IonItem
+              style={{
+                border: "1px solid black",
+                marginBottom: "5px",
+                margin: "auto",
+                width: "300px",
+              }}
+            >
+              <IonLabel>Email</IonLabel>
+              <IonInput type="email"></IonInput>
+            </IonItem>
 
+            <IonItem className="pass" id="pas">
+              <IonLabel>Password</IonLabel>
+              <IonInput type="password"></IonInput>
+            </IonItem>
+            <IonButton type="submit" style={{ marginLeft: "150px" }}>
+              Sign up
+            </IonButton>
+          </IonList>
+        </form>
+        <div id="text">
+          <a href="/login">Already have account?</a>
+        </div>
       </IonContent>
     </IonPage>
   );
