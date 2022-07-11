8165fd0ec514d81e454519f30db51b483d4b4192
Digbijay Nayak <digbijaynayak522@gmail.com>
2022-07-11 13:07:05 +0530

    Added other funtionality

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.medionptg.app',
   appName: 'MediON',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    "url": "http://172.17.6.27:8100",
+    "cleartext": true
+  }
 };
 
 export default config;
