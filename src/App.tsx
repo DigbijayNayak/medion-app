3e617e2dbdccda47332917f3da5dd579adb6876e
Digbijay Nayak <digbijaynayak522@gmail.com>
2022-07-12 17:23:50 +0530

    Added app update features

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import {
   IonLoading,
   IonPage,
   IonRouterOutlet,
+  isPlatform,
   setupIonicReact,
+  useIonAlert,
+  useIonToast,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 
@@ -25,36 +28,113 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
-/* Theme variables */
 import "./theme/variables.css";
-// import SearchPage from "./pages/Search";
-// import WishlistPage from "./pages/Wishlist";
-// import ProfliePage from "./pages/Profile";
-// import HomePage from "./pages/Home";
-// import CartPage from "./pages/Cart";
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import ResetPasswordPage from "./pages/ResetPassword";
 import AppStack from "./pages/AppStack";
 import { AuthContext } from "./auth";
-import { auth } from "./firebase";
+import { auth, db } from "./firebase";
+import {App as app} from "@capacitor/app"
+import {doc, getDoc} from "firebase/firestore"
 import { onAuthStateChanged } from "firebase/auth";
+import { Browser } from '@capacitor/browser';
 import { useEffect, useState } from "react";
 setupIonicReact();
 
 const App: React.FC = () => {
   // const authState = useAuthInit();
   // const {loggedIn} = useAuth();
+
+  const [update, setUpdate] = useState<any>({});
+  const [appVersion, setAppVersion] = useState<any>("");
+  const [presentAlert] = useIonAlert();
+  const [present] = useIonToast();
+
+
+  const handleToast = (msg:any) => {
+    present({
+      message: msg,
+      position: "top",
+      animated: true,
+      duration: 2000,
+      color: "success",
+      mode: "md"
+    })
+  }
+
+  const handleAlert = (msg:any, title:any, btn:any, appVersion:any) => {
+    presentAlert({
+      header: title,
+      subHeader: `Version: ${appVersion}`,
+      message: msg,
+      buttons: [
+        {
+          text: btn,
+          role: "Download",
+          handler: async () => {
+            handleToast("Download Clicked");
+            await Browser.open({
+              url: "https://play.google.com/store/apps/details?id=com.medionptg.app"
+            })
+          }
+        }
+      ]
+    })
+  }
+
+  const getAppInfo = async () => {
+    let info = await app.getInfo();
+    return info;
+  }
+
+  const getConfigData = async () => {
+    const updateRef = doc(db, "medion-app-config", "qvfcOEk0qbit4oD78Z40")
+    const docSnap = await getDoc(updateRef);
+    if(docSnap.exists()){
+      const data = docSnap.data();
+      console.log("Document data: ", docSnap.data());
+      setUpdate(data.updateMsg);
+      setAppVersion(data.current_version);
+    }else{
+      console.log("No such document!");
+    }
+  }
+
+  const checkUpdate = async () => {
+    try{
+      if(isPlatform("android")){
+        const currentAppInfo = getAppInfo();
+        if(appVersion > (await currentAppInfo).version){
+          const msg = update.msg;
+          const title = update.title;
+          const btn = update.btn;
+          handleAlert(msg, title, btn, appVersion);
+        }
+      }else{
+        const msg = "App is not running on android platform";
+        handleToast(msg);
+      }
+    }
+    catch(error){
+      handleToast(error);
+    }
+  }
+
   const [authState, setAuthState] = useState({
     loading: true,
     loggedIn: false,
   });
   useEffect(() => {
+    getConfigData();
+    getAppInfo();
     onAuthStateChanged(auth, (user) => {
       // setLoggedIn(Boolean(user));
       setAuthState({ loading: false, loggedIn: Boolean(user) });
     });
-  }, []);
+  }, [0]);
+
+  checkUpdate();
   console.log(`rendering App with authState:`, authState);
   if (authState.loading) {
     return <IonLoading isOpen />;
